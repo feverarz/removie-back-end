d48392e9248f467b310d40d767a9e4af0313f102
feverarz <fevera.rz@gmail.com>
2025-09-27 03:38:04 -0300

    Corrijo rutas y nombre del DLL para Railway

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Compila el proyecto en modo Release y publica los binarios en ./out
-dotnet publish ./Rimovie/Rimovie.csproj -c Release -o out
-
-# Ejecuta el DLL generado
-dotnet out/Rimovie.dll
+dotnet publish ./Rimovie/Rimovie.csproj -c Release -o ./Rimovie/out
+dotnet ./Rimovie/out/Rimovie.dll
